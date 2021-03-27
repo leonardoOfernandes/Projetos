4f85c700a00c2bdcb82a35d11fbc2793e8b404fb
leonardoOfernandes <leonardo021290@gmail.com>
2021-03-27 01:19:09 -0300

    PUT, DELETE e GET para Cliente

--- Curso_Projeto_BackEnd/src/main/java/com/cursomc/resources/ClienteResource.java
+++ Curso_Projeto_BackEnd/src/main/java/com/cursomc/resources/ClienteResource.java
@@ -1,16 +1,20 @@
 package com.cursomc.resources;
 
-import com.cursomc.domain.Categoria;
 import com.cursomc.domain.Cliente;
-import com.cursomc.services.CategoriaService;
+import com.cursomc.domain.Cliente;
+import com.cursomc.dto.ClienteDTO;
+import com.cursomc.services.ClienteService;
 import com.cursomc.services.ClienteService;
 import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -24,5 +28,44 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() throws ObjectNotFoundException {
+		List<ClienteDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@RequestMapping(value = "/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findAllByPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+															@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+															@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+															@RequestParam(value = "direction", defaultValue = "ASC") String direction) throws ObjectNotFoundException {
+		Page<Cliente> list = service.findAllByPage(page, linesPerPage, orderBy, direction);
+
+		Page<ClienteDTO> listDto = list.map(Cliente -> new ClienteDTO(Cliente));
+
+		return ResponseEntity.ok(listDto);
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO obj) throws ObjectNotFoundException{
+		Cliente response = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(response.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id,@Valid @RequestBody ClienteDTO obj) throws ObjectNotFoundException{
+		Cliente response = service.update(id, obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) throws ObjectNotFoundException {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 
 }
--- Curso_Projeto_BackEnd/src/main/java/com/cursomc/services/ClienteService.java
+++ Curso_Projeto_BackEnd/src/main/java/com/cursomc/services/ClienteService.java
@@ -1,14 +1,21 @@
 package com.cursomc.services;
 
-import com.cursomc.domain.Categoria;
 import com.cursomc.domain.Cliente;
-import com.cursomc.repositories.CategoriaRepository;
+import com.cursomc.domain.Cliente;
+import com.cursomc.dto.ClienteDTO;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.repositories.ClienteRepository;
 import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ClienteService {
@@ -21,5 +28,51 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", Tipo: "+ Cliente.class.getName()));
 	}
+	public Cliente insert(ClienteDTO obj){
+		obj.setId(null);
+		return repo.save(fromDTO(obj));
+	}
 
+	public Cliente update(Integer id,ClienteDTO obj) throws ObjectNotFoundException {
+		obj.setId(id);
+		Cliente cliente = find(id);
+		updateData(cliente, obj);
+		return repo.save(cliente);
+	}
+
+	private void updateData(Cliente cliente, ClienteDTO obj) {
+		cliente.setNome(obj.getNome());
+		cliente.setEmail(obj.getEmail());
+
+	}
+
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException exception){
+			throw new DataIntegrityViolationException("Não é possivel excluir um cliente porque há entidades relacionadas");
+		}
+
+	}
+
+	public List<ClienteDTO> findAll(){
+		List<Cliente> list = repo.findAll();
+		List<ClienteDTO> listDto = list.stream().map(Cliente -> new ClienteDTO(Cliente)).collect(Collectors.toList());
+		return listDto;
+	}
+
+	public Page<Cliente> findAllByPage(Integer page,
+										 Integer linesPerPage,
+										 String orderBy,
+										 String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto){
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
 }
