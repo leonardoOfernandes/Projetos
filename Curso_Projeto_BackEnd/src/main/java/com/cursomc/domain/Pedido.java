7fbd945a4efa3061eef59a3433c8f922a079f370
leonardoOfernandes <leonardo021290@gmail.com>
2021-05-06 00:12:45 -0300

    Subtotal e total do Pedido

--- Curso_Projeto_BackEnd/src/main/java/com/cursomc/domain/Pedido.java
+++ Curso_Projeto_BackEnd/src/main/java/com/cursomc/domain/Pedido.java
@@ -94,6 +94,10 @@ public class Pedido implements Serializable {
         this.itens = itens;
     }
 
+    public double getValorTotal(){
+        return itens.stream().mapToDouble(ItemPedido::getSubTotal).sum();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
