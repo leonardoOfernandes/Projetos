f08568b9e3209c9b79b4ea3c667677f0893a4370
leonardoOfernandes <leonardo021290@gmail.com>
2021-07-01 19:26:27 -0300

    Adjustando projeto

--- Curso_Projeto_BackEnd/src/main/java/com/cursomc/repositories/CidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cursomc.repositories;
-
-import com.cursomc.domain.Categoria;
-import com.cursomc.domain.Cidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
-}
-
--- Curso_Projeto_BackEnd/src/main/java/com/cursomc/repositories/EstadoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cursomc.repositories;
-
-import com.cursomc.domain.Cidade;
-import com.cursomc.domain.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadoRepository  extends JpaRepository<Estado, Integer> {
-}
